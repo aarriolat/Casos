5252f4932fa909fa304a1876176e4cc1c5ad2971
ErnestoRMC <72056498+ErnestoRMC@users.noreply.github.com>
2020-10-03 08:05:34 -0600

    Agregar archivos de proyecto.

---
+++ GestordeCasosPF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestordeCasosPF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
